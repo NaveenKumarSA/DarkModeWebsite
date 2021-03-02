6e9bc5a4514bfda998d80ac8b9b55bd2bd3414a1
NaveenKumarSA <57409012+NaveenKumarSA@users.noreply.github.com>
2021-03-02 12:07:49 +0530

    backup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <!-- Custom styles for this template -->
-    <link href="css\app.css" rel="stylesheet">
+    <link href="css\app`.css" rel="stylesheet">
 
 </head>
 <body>
